8773bb18c2a163b7a552efbca4774623f8cfabf3
kubitz <noe.guisset@gmail.com>
2020-01-27 17:49:20 +0000

    added docstrings on function of class GeigerMeter

--- sensor.py
+++ sensor.py
@@ -6,6 +6,11 @@ import time
 
 class GeigerMeter: 
     def __init__(self, pin_signal = 5, pin_noise = 4): 
+        """ Constructor for Geiger Meter
+            Sets up the GPIO interrupt on the signal pin
+            input: pin numbers (in BCM)
+        """
+
         self.pin_signal = pin_signal
         self.pin_noise = pin_noise
         self.radiation_level = self.read_radiation()
@@ -15,15 +20,27 @@ class GeigerMeter:
         GPIO.add_event_detect(self.pin_signal,GPIO.FALLING, callback = self._geiger_pulse_handler, bouncetime = 70)
     
     def read_radiation(self): 
+        """ Function returns measured radiation level
+            input: cpm (count per minute)
+            output: radiation level in Sievets
+        """
          self.radiation_level = randint(1, 200)
          self.uncertainty = randint(1,300)
          return self.radiation_level
 
     def _geiger_pulse_handler(self):
+        """ Function called on interrupt of Geiger Sensor
+            It increment the pulse count by one
+        """
+
         self._cpm += 1
 
-
     def format_data(self): 
+        """ Function formats sensor data in a JSON file
+            input: radiation level and uncertainty
+            output: json file
+        """
+
         data = {'radiation': self.radiation_level
                 ,'uncertainty': self.uncertainty
                     }
@@ -31,6 +48,10 @@ class GeigerMeter:
         return data_json
     
     def __del__(self): 
+        """ Destructor for the Geiger sensor
+            Cleans up GPIO after deleting member of GeigerMeter Class
+        """
+
         print("Sensor Deleted!")
         GPIO.cleanup()
 
