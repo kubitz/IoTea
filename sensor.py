7919b57a6c219b0cd5fd76e1f51e42c13784b10b
kubitz <noe.guisset@gmail.com>
2020-01-28 09:37:42 +0000

    added millis function time management

--- sensor.py
+++ sensor.py
@@ -4,21 +4,35 @@ import json
 import paho.mqtt as mqtt
 import time 
 
+GEIGER_VALUE = 53.032
+
+
 class GeigerMeter: 
-    def __init__(self, pin_signal = 5, pin_noise = 4): 
+    def __init__(self, pin_signal = 5, pin_noise = 4, bouncetime = 70): 
         """ Constructor for Geiger Meter
             Sets up the GPIO interrupt on the signal pin
             input: pin numbers (in BCM)
         """
-
+        self.bouncetime = bouncetime
         self.pin_signal = pin_signal
         self.pin_noise = pin_noise
         self.radiation_level = self.read_radiation()
         self.uncertainty = 0
+        
+        self._pulse_count = 0
         self._cpm = 0
+        self._previous_time = GeigerMeter.millis()
+        self._current_time = GeigerMeter.millis()
+        
         GPIO.setup(self.pin_signal, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
-        GPIO.add_event_detect(self.pin_signal,GPIO.FALLING, callback = self._geiger_pulse_handler, bouncetime = 70)
+        GPIO.add_event_detect(self.pin_signal,GPIO.FALLING, callback = self._geiger_pulse_handler, bouncetime = self.bouncetime)
     
+    @staticmethod
+    def millis(): 
+        millis = int(round(time.time() * 1000))
+        return millis
+
+
     def read_radiation(self): 
         """ Function returns measured radiation level
             input: cpm (count per minute)
@@ -32,8 +46,8 @@ class GeigerMeter:
         """ Function called on interrupt of Geiger Sensor
             It increment the pulse count by one
         """
-
-        self._cpm += 1
+        self._pulse_count += 1
+        self._current_time = GeigerMeter.millis()
 
     def format_data(self): 
         """ Function formats sensor data in a JSON file
