c6004fe3c2c1e0dc61c588be72db10bdbf83691a
kubitz <noe.guisset@gmail.com>
2020-01-30 12:18:55 +0000

    added encryption

--- mqtt.py
+++ mqtt.py
@@ -1,6 +1,8 @@
 import paho.mqtt.client as mqtt
-# This is test 
-client = mqtt.Client()
-client.connect("test.mosquitto.org", port=1883)
+import ssl 
+client.tls_set(ca_certs="mosquitto.org.crt",
+certfile="client.crt",keyfile="client.key",tls_version=ssl.PROTOCOL_
+TLSv1_2)client = mqtt.Client()
+client.connect("test.mosquitto.org", port=8884)
 client.publish("IC.embedded/IoTea/test", "hello")
 mqtt.error_string(MSG_INFO.rc)
