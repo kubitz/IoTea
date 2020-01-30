a3a70a440330bb893a6729b2ce9e338c445a6127
kubitz <noe.guisset@gmail.com>
2020-01-30 12:21:36 +0000

    added lf line spacing

--- mqtt.py
+++ mqtt.py
@@ -2,5 +2,5 @@ import paho.mqtt.client as mqtt
 # This is test 
 client = mqtt.Client()
 client.connect("test.mosquitto.org", port=1883)
-client.publish("IC.embedded/IoTea/test", "hello")
+client.publish("IC.embedded/IoTea/test", "the government is corrupt. FEAR ")
 mqtt.error_string(MSG_INFO.rc)
