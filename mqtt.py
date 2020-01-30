97ad4faee16a543222ef3a55fe4312872191e20f
kubitz <noe.guisset@gmail.com>
2020-01-30 11:55:38 +0000

    added basic test of mqtt

--- mqtt.py
+++ mqtt.py
@@ -1,1 +1,6 @@
-imp
+import paho.mqtt.client as mqtt
+# This is test 
+client = mqtt.Client()
+client.connect("test.mosquitto.org", port=1883)
+client.publish("IC.embedded/IoTea/test", "hello")
+mqtt.error_string(MSG_INFO.rc)
