35292559541d834aa191be24030746cfbb1eb81f
kubitz <noe.guisset@gmail.com>
2020-02-02 17:15:29 +0000

    fixed another intend issue

--- embedded/record.py
+++ embedded/record.py
@@ -7,7 +7,7 @@ form_1 = pyaudio.paInt16
 chans=1
 samp_rate = 44100
 chunk = 4096
-record_secs = 25     #record time
+record_secs = 10     #record time
 dev_index = 2
 wav_output_filename = 'test1.wav'
 
@@ -19,7 +19,7 @@ stream=audio.open(format = form_1,rate=samp_rate,channels=chans, input_device_in
 print("recording")
 frames=[]
 
-for ii in range(0,int((samp_rate/chunk)*record_secs)):N
+for ii in range(0,int((samp_rate/chunk)*record_secs)):
     data=stream.read(chunk,exception_on_overflow = False)
     frames.append(data)
 
