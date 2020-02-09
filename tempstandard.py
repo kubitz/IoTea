083756cac4fe2ac1c363af86b98bfb88a0af51d2
kubitz <noe.guisset@gmail.com>
2020-02-09 21:09:06 +0000

    Fixed standard

--- tempstandard.py
+++ tempstandard.py
@@ -7,19 +7,19 @@ class Temp():
     def __init__(self):
         base_dir = '/sys/bus/w1/devices/'
         device_folder = glob.glob(base_dir + '28*')[0]
-        device_file = device_folder + '/w1_slave'
+        self.device_file = device_folder + '/w1_slave'
 
 
-    def read_temp_raw():
-        f = open(device_file, 'r')
+    def read_temp_raw(self):
+        f = open(self.device_file, 'r')
         lines = f.readlines()
         f.close()
         return lines
 
-    def read_temp():
-        lines = read_temp_raw()
+    def read_temp(self):
+        lines = self.read_temp_raw()
         while lines[0].strip()[-3:] != 'YES':
-            lines = read_temp_raw()
+            lines = self.read_temp_raw()
         equals_pos = lines[1].find('t=')
         if equals_pos != -1:
             temp_string = lines[1][equals_pos + 2:]
@@ -27,7 +27,7 @@ class Temp():
             return temp
 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
     myTemp = Temp()
     while True:
         print(myTemp.read_temp())
