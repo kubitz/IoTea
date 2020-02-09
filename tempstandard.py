2f0860b095872598cd775ce49436e710229eb67a
luccaseagles <48408154+luccaseagles@users.noreply.github.com>
2020-02-09 21:03:15 +0000

    better

--- tempstandard.py
+++ tempstandard.py
@@ -2,30 +2,33 @@ import glob
 import time
 
 
+class Temp():
 
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
-
-
-def read_temp_raw():
-    f = open(device_file, 'r')
-    lines = f.readlines()
-    f.close()
-    return lines
+    def __init__(self):
+        base_dir = '/sys/bus/w1/devices/'
+        device_folder = glob.glob(base_dir + '28*')[0]
+        device_file = device_folder + '/w1_slave'
 
 
-def read_temp():
-    lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
+    def read_temp_raw():
+        f = open(device_file, 'r')
+        lines = f.readlines()
+        f.close()
+        return lines
+
+    def read_temp():
         lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos + 2:]
-        temp = float(temp_string) / 1000.0
-        return temp
+        while lines[0].strip()[-3:] != 'YES':
+            lines = read_temp_raw()
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos + 2:]
+            temp = float(temp_string) / 1000.0
+            return temp
 
 
-while True:
-    print(read_temp())
-    time.sleep(1)
+if __name__ == "__main__"
+    myTemp = Temp()
+    while True:
+        print(myTemp.read_temp())
+        time.sleep(1)
