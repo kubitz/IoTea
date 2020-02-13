bc41c915e8376231aa74e8272cdb023cc546f172
luccaseagles <48408154+luccaseagles@users.noreply.github.com>
2020-02-13 14:13:16 +0000

    Final

--- frontend/frontend2.py
+++ frontend/frontend2.py
@@ -7,19 +7,27 @@ import json
 
 app = dash.Dash(__name__)
 
+#Define HTML layout of app
 app.layout = html.Div(children=[
     html.Div([
         html.H4('IoTea.', id='title'),
         html.Div(id='live-update-text'),
-        html.Div(dcc.Input(id='input-box', type='text')),
-        html.Button('Submit', id='button'),
-        html.Div(id='output-container-button',
-                 children='Enter a value and press submit'),
         dcc.Interval(
             id='interval-component',
             interval=1*1000, # in milliseconds
             n_intervals=0
         ),
+        html.P(
+            "  Tang Dynasty scholars believe the Gautama Buddha accidentally fell asleep meditating in front of a wall for nine years."
+            " He woke up in such disgust at his weakness that he cut off his own eyelids."
+            " They fell to the ground and took root, growing into tea bushes."
+            " Legend says those who brew the fruit of the eyelids unlock the ability to memorise phone numbers with a 30% success rate.", id='blurb1'),
+        # html.P(" Tea has played a significant role in Asian culture for centuries as a staple beverage and a curative."
+        #        " At IoTea, we see it is a status symbol and a conversation starter."
+        #        " Laud over your uncultured colleagues as you wrap your clammy hands around your boring novelty mug.", id='blurb2'),
+        html.P(" In a world full of superfluous devices targeted at the unoriginal, that profit off your information and naïvety, you have found the only IoT device that makes you an individual."
+               " Indulge yourself in this authentic moment. Laud over your uncultured colleagues as you wrap your clammy hands around your boring novelty mug.. Maybe consider buying an IoT toothbrush next?"
+               " Brushing your teeth can be quite the complicated ordeal.", id='blurb3'), #EDIT
         dcc.Checklist(
             options=[
                 {'label': 'I agree to the terms and conditions', 'value': 'NYC'},
@@ -31,6 +39,7 @@ app.layout = html.Div(children=[
     ]
 )
 
+#Update the data to be displayed
 @app.callback(Output('live-update-text', 'children'),
               [Input( 'interval-component', 'n_intervals')])
 def update_metrics(n):
@@ -64,21 +73,7 @@ def update_metrics(n):
     return output
 
 
-@app.callback(
-    dash.dependencies.Output('output-container-button', 'children'),
-    [dash.dependencies.Input('button', 'n_clicks')],
-    [dash.dependencies.State('input-box', 'value')])
-def update_output(n_clicks, value):
-    username = {}
-    username['user'] = value
-
-    with open('output.txt', 'w') as file:
-        file.write(json.dumps(username))
-
-    return 'The input value was "{}" and the button has been clicked {} times'.format(value, n_clicks)
-
-
-# Multiple components can update everytime interval gets fired.
+#Function to update graph
 @app.callback(Output('live-update-graph', 'figure'),
               [Input('interval-component', 'n_intervals')])
 def update_graph_live(n):
@@ -95,22 +90,27 @@ def update_graph_live(n):
         x = [0]
         y = [0]
 
-
     fig = {
         "data": [{"type": "line",
-                  "x":x ,
-                  "y":y}],
-        "layout": {"title": {"text": "Temperature vs. Time"},
+                  "x": x,
+                  "y": y,
+                 'line': {'width': 0.5, 'color': 'black'}
+                  }],
+        "layout": {
+                   "title": {"text": "Temperature vs. Time"},
                    'xaxis': {'title': "Time(s)"},
                    'yaxis': {'title': "Temperature(C)"}
+
                    }
     }
 
     return fig
 
+#Main to launch frontend
 if __name__ == '__main__':
     #open a webbrowser to the default IP adress
     webbrowser.open( 'http://127.0.0.1:8050/', new=2)
+
     #Launch the server
     app.run_server(debug=True)
 
